92c88c58839df06cd0f395e74c5f015b60a37846
Syed Moin <syed.s@accolite.com>
2022-12-06 17:50:21 +0530

    The forked project is modified

--- src/app/modules/vendor/active-vendor/active-vendor.component.ts
+++ src/app/modules/vendor/active-vendor/active-vendor.component.ts
@@ -1,4 +1,8 @@
+import { R3DelegatedFnOrClassMetadata } from '@angular/compiler/src/render3/r3_factory';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-active-vendor',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActiveVendorComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private fb: FormBuilder) { }
+  regForm = this.fb.group({
+    uname: ['',Validators.required],
+    uemail: ['',Validators.required],
+    ubio: ['',Validators.required],
+    udomain: ['',Validators.required]
+  })
+  
   ngOnInit(): void {
   }
 
+  val! :any
+  onSubmit(){
+    console.log(this.regForm.value);
+    this.val=this.regForm.value;
+  }
 }
